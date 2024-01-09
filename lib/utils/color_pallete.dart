36859875bfc32ea986545aafec3295c9dd388bd7
rYuma <prijeshshahi0@gmail.com>
2024-01-09 22:17:11 +0545

    font improved and shadows added

--- lib/utils/color_pallete.dart
+++ lib/utils/color_pallete.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Color backgroundColor =Color.fromARGB(255, 39, 142, 141);
+  static const Color shadowColor =Color.fromARGB(255, 15, 81, 80);
   static const Color defaultTileColor =Color.fromARGB(255, 177, 243, 222);
   static const Color checkedTileColor =Color.fromARGB(255, 226, 187, 165);
   static const Color todayTextColor =Color.fromARGB(255,251, 209, 136);
--- lib/utils/note_tile.dart
+++ lib/utils/note_tile.dart
@@ -10,11 +10,13 @@ class ToDoTile extends StatelessWidget {
     required this.taskCompletion,
     this.onChanged,
     this.deleteFunction,
+    this.editFunction,
   });
   final String taskName;
   final bool taskCompletion;
   final Function(bool?)? onChanged;
   final Function(BuildContext)? deleteFunction;
+  final Function(BuildContext)? editFunction;
 
   String getDate() {
     final date = DateTime.now();
@@ -31,18 +33,39 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.all(12.0).copyWith(left: 20,right: 20),
       child: Slidable(
-        endActionPane: ActionPane(motion: StretchMotion(), children: [
-          SlidableAction(
-            onPressed: deleteFunction,
-            icon: Icons.delete,
-            backgroundColor: Colors.red,
-          )
-        ]),
+        closeOnScroll: true,
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              borderRadius: BorderRadius.circular(10),
+              autoClose: true,
+              onPressed: editFunction,
+              icon: Icons.edit,
+              backgroundColor: AppColors.todayTextColor,
+            ),
+            SlidableAction(
+              borderRadius: BorderRadius.circular(10),
+              autoClose: true,
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+            ),
+          ],
+        ),
         child: Container(
           padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.shadowColor.withOpacity(0.6),
+                spreadRadius: 3,
+                blurRadius: 1,
+                offset: Offset(0, 4),
+              )
+            ],
             color: taskCompletion
                 ? AppColors.checkedTileColor
                 : AppColors.defaultTileColor,
@@ -56,7 +79,8 @@ class ToDoTile extends StatelessWidget {
                   Text(
                     taskName,
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                       decoration: taskCompletion
                           ? TextDecoration.lineThrough
                           : TextDecoration.none,
@@ -72,7 +96,10 @@ class ToDoTile extends StatelessWidget {
                       taskCompletion
                           ? "Task Completed at ${getTime()}"
                           : getDate(),
-                      style: const TextStyle(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16
+                      ),
                     ),
                   ),
                 ],
